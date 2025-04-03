1582d2a064fb6815b739058d5e33322dfe349af7
Vwore <vanshaj.wore@gmail.com>
2025-04-03 14:19:41 +0530

    project v1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+
+const dictionary = [
+  {
+    word: "React",
+    meaning: "A JavaScript library for building user interfaces.",
+  },
+
+  { word: "Component", meaning: "A reusable building block in React." },
+
+  { word: "State", meaning: "An object that stores data for a component." },
+];
 
 function App() {
+  const [word, setWord] = useState();
+  const [definition, setDefinition] = useState("");
+  function handleSearch(word) {
+    const i = dictionary.findIndex(
+      (val) => val.word.toLowerCase() === word.toLowerCase()
+    );
+    if (i !== -1) setDefinition(dictionary[i].meaning);
+    else setDefinition("Word not found in the dictionary.");
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Dictionary App</h1>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          handleSearch(word);
+        }}
+      >
+        <input
+          placeholder="Search for a word..."
+          value={word}
+          onChange={(e) => {
+            setWord(e.target.value);
+          }}
+        ></input>
+        <button type="submit">Search</button>
+      </form>
+      <h3>Definition:</h3> <p>{definition}</p>
     </div>
   );
 }
